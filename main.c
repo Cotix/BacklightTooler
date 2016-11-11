6c458086ba7253f80c2c21c659b9ffb9aea74c89
cotix <n.goldsmid@student.utwente.nl>
2016-11-11 18:11:50 +0100

    Very important pulse functionality

--- main.c
+++ main.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #define MAX_BRIGHTNESS 416
 #define MIN_BRIGHTNESS 0
 #define PATH "/sys/class/backlight/intel_backlight/brightness"
 int getBrightness();
+void setBrightness(int target);
 
 void inc() {
-	char buffer[256];
 	int brightness = getBrightness() + 10;
 	brightness = brightness > MAX_BRIGHTNESS ? MAX_BRIGHTNESS : brightness;
-	sprintf(buffer, "%i", brightness);
-	FILE* file = fopen(PATH, "w");
-	fwrite(buffer, strlen(buffer), 1, file);
-	fclose(file);
+    setBrightness(brightness);
 }
 
 void dec() {
-    char buffer[256];
     int brightness = getBrightness() - 10;
     brightness = brightness < MIN_BRIGHTNESS ? MIN_BRIGHTNESS : brightness;
-    sprintf(buffer, "%i", brightness);
+    setBrightness(brightness);
+}
+
+void setBrightness(int target) {
+	char buffer[256];
+	sprintf(buffer, "%i", target);
 	FILE* file = fopen(PATH, "w");
     fwrite(buffer, 4, 1, file);
 	fclose(file);
+}
+
+void pulse() {
+    int low = 1;
+    int high = MAX_BRIGHTNESS;
+    int t;
+    int counter = 30;
+    while (counter --> 0){
+        for (t = low; t < high; ++t) {
+            setBrightness(t);
+            usleep(2000);
+        }
+        for (t = high; t > low; --t) {
+            setBrightness(t);
+            usleep(2000);
+        }
+    }
 }
 
 int getBrightness() {
@@ -38,4 +57,5 @@ int main(int argc, char **argv) {
 	if (argc != 2) return 1;
 	if (!strcmp(argv[1], "inc")) inc();
 	if (!strcmp(argv[1], "dec")) dec();
+	if (!strcmp(argv[1], "pulse")) pulse();
 }
