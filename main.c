0129846d3e812043b384132ae1ab73f8f54af3ac
cotix <n.goldsmid@student.utwente.nl>
2016-11-11 20:06:53 +0100

    Add set functionality

--- main.c
+++ main.c
@@ -56,16 +56,17 @@ int getBrightness() {
 int main(int argc, char **argv) {
 	if (argc < 2) {
         puts("Usage: BacklightTool OPTION [AMOUNT]\n"\
-             "Options: inc, dec, pulse");
+             "Options: set, inc, dec, pulse");
         return 0;
     }
     int amount = DEFAULT_AMOUNT;
     if (argc > 2) amount = atoi(argv[2]);
-    if (amount < 0 || amount > MAX_BRIGHTNESS) {
+    if (amount <= 0 || amount > MAX_BRIGHTNESS) {
         puts("Invalid amount.\n");
         return 0;
     }
 	if (!strcmp(argv[1], "inc")) inc(amount);
 	if (!strcmp(argv[1], "dec")) dec(amount);
 	if (!strcmp(argv[1], "pulse")) pulse(amount);
+    if (!strcmp(argv[1], "set") && argc > 2) setBrightness(amount);
 }
